5daf8df5de0e29094b3dbb156d6a5c33771f7abe
PedritoRamirez <p_a_ramirez@hotmail.com>
2024-03-10 07:28:21 -0300

    Desafio2-Condicionales-JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// primer problema desafio Condicionales JavaScript
+let click = false;
+// ************************************************
+
+// Segundo problema desafio Condicionales JavaScript
+ var caja1 = 0;
+ var caja2 = 0;
+ var caja3 = 0;
+ var total = 0;
+// ************************************************
+
+// Tercer problema desafio Condicionales JavaScript
+  var password = 0;
+  var pass1 = 0;
+  var pass2 = 0;
+  var pass3 = 0;
+// ************************************************
